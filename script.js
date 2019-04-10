f2db31988025f19233253394daf3f7ad79d641db
Pedro Miguel <pedrom888@gmail.com>
2019-04-10 15:28:41 +0700

    display inside box

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 $('form').on('submit', function(e) {
 	e.preventDefault(); // it prevents the default behaviour
 	console.log($('#anymessage').val());
+  $('#box').html($('#anymessage').val());
 });
