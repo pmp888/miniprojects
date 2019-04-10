edcb7407380a567231bb7422f92d4bca2321cd38
Pedro Miguel <pedrom888@gmail.com>
2019-04-10 15:43:21 +0700

    add break between inputs

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $('form').on('submit', function(e) {
 	e.preventDefault(); // it prevents the default behaviour
 	console.log($('#anymessage').val());
-  $('#box').html($('#anymessage').val());
+  $('#box').append($('#anymessage').val() + '<br>');
 });
